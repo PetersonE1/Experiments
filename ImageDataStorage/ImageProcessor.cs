7c0ee56e47c8e81d1ee8bf98b194e50d1d69bc26
PetersonE1 <PetersonE_1@live.com>
2022-11-03 19:59:50 -0400

    documentation

--- ImageDataStorage/ImageProcessor.cs
+++ ImageDataStorage/ImageProcessor.cs
@@ -15,6 +15,10 @@ namespace ImageDataStorage
         public const uint X3 = 256u * 256u * 256u;
 
         // Item Conversions
+
+        /// <summary>
+        /// Converts a color into an unsigned integer using tiered steps.
+        /// </summary>
         static uint ConvertToInt(this Color c)
         {
             uint result = 0;
@@ -25,6 +29,9 @@ namespace ImageDataStorage
             return result;
         }
 
+        /// <summary>
+        /// Converts an unsigned integer into a color using tiered steps.
+        /// </summary>
         static Color ConvertToColor(this uint i)
         {
             int R, G, B, A = 0;
@@ -42,6 +49,10 @@ namespace ImageDataStorage
         }
 
         // Data Conversions
+
+        /// <summary>
+        /// Converts an image to an array of unsigned integers from the given filepath.
+        /// </summary>
         public static uint[] ConvertImageToArray(string filePath)
         {
             if (!File.Exists(filePath)) return null;
@@ -66,6 +77,33 @@ namespace ImageDataStorage
             return imageData;
         }
 
+        /// <summary>
+        /// Converts an image to an array of unsigned integers from the given bitmap.
+        /// </summary>
+        public static uint[] ConvertImageToArray(Bitmap image)
+        {
+            Console.WriteLine(image.Height);
+            int imageSize = image.Height * image.Width;
+            uint[] imageData = new uint[imageSize];
+
+            int i = 0;
+            for (int h = 0; h < image.Height; h++)
+            {
+                for (int w = 0; w < image.Width; w++)
+                {
+                    Color c = image.GetPixel(w, h);
+                    imageData[i] = image.GetPixel(w, h).ConvertToInt();
+                    i++;
+                }
+            }
+            image.Dispose();
+
+            return imageData;
+        }
+
+        /// <summary>
+        /// Converts an unsigned integer array into a bitmap.
+        /// </summary>
         public static Bitmap ConvertToImage(uint[] data) {
             List<int> heights = new List<int>();
             for (int i = 0; i < data.Length; i++)
@@ -97,6 +135,9 @@ namespace ImageDataStorage
             return map;
         }
 
+        /// <summary>
+        /// Converts an unsigned integer array into a bitmap of the given height if possible. Otherwise, defaults to the minimum height.
+        /// </summary>
         public static Bitmap ConvertToImage(uint[] data, int attemptHeight)
         {
             List<int> heights = new List<int>();
@@ -125,6 +166,10 @@ namespace ImageDataStorage
         }
 
         // Wrapped Methods
+
+        /// <summary>
+        /// Generates a .png image from an unsigned integer array at the given filepath.
+        /// </summary>
         public static void GenerateImageFromFile(string inputPath, string outputPath)
         {
             if (!File.Exists(inputPath)) return;
@@ -134,6 +179,9 @@ namespace ImageDataStorage
             map.Save(outputPath + $"-H{map.Height}.png");
         }
 
+        /// <summary>
+        /// Generates a .png image of the given height from an unsigned integer array at the given filepath, if possible. Otherwise, defaults to the minimum height.
+        /// </summary>
         public static void GenerateImageFromFile(string inputPath, string outputPath, int targetHeight)
         {
             if (!File.Exists(inputPath)) return;
@@ -143,18 +191,27 @@ namespace ImageDataStorage
             map.Save(outputPath + $"-H{map.Height}.png");
         }
 
+        /// <summary>
+        /// Generates a .png image from a given unsigned integer array.
+        /// </summary>
         public static void GenerateImageFromArray(uint[] array, string outputPath)
         {
             Bitmap map = ConvertToImage(array);
             map.Save(outputPath + $"-H{map.Height}.png");
         }
 
+        /// <summary>
+        /// Generates a .png image of the given height from a given unsigned integer array, if possible. Otherwise, defaults to the minimum height.
+        /// </summary>
         public static void GenerateImageFromArray(uint[] array, string outputPath, int targetHeight)
         {
             Bitmap map = ConvertToImage(array, targetHeight);
             map.Save(outputPath + $"-H{map.Height}.png");
         }
 
+        /// <summary>
+        /// Saves an unsigned integer array to a file.
+        /// </summary>
         public static void SaveArrayToFile(uint[] array, string outputPath)
         {
             string data = string.Join('\n', array);
@@ -162,12 +219,19 @@ namespace ImageDataStorage
         }
 
         // Data Processing
+
+        /// <summary>
+        /// Prompts the user for input. If the response matches a value, return 'true' and output the response.
+        /// </summary>
         private static bool ReadIfContains(string[] values, out string n)
         {
             n = Console.ReadLine();
             return values.Contains(n);
         }
 
+        /// <summary>
+        /// Prompts the user to choose a valid height of a bitmap given an array.
+        /// </summary>
         public static int AskHeight(int length)
         {
             List<int> heights = new List<int>();
@@ -188,25 +252,38 @@ namespace ImageDataStorage
         }
 
         // Bonus Methods
+
+        /// <summary>
+        /// Checks if 'a' is a factor of 'b'.
+        /// </summary>
         public static bool IsFactor(int a, int b)
         {
             if (a != 0 && b % a == 0) return true;
             return false;
         }
 
+        /// <summary>
+        /// Checks if a given length and height will produce a valid bitmap.
+        /// </summary>
         public static bool IsValid(int a, int b)
         {
             if (IsFactor(a, b) && a < 65536 && b / a < 65536) return true;
             return false;
         }
 
-        public static bool IsSquarable(int a)
+        /// <summary>
+        /// Checks if there is an integer root of 'a'.
+        /// </summary>
+        public static bool IsRootable(int a)
         {
             int b = (int)Math.Sqrt(a);
             if (a == b * b) return true;
             return false;
         }
 
+        /// <summary>
+        /// Scales a given bitmap.
+        /// </summary>
         public static Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
         {
             Bitmap result = new Bitmap(width, height);
@@ -218,9 +295,12 @@ namespace ImageDataStorage
             return result;
         }
 
+        /// <summary>
+        /// Returns the integer root of a number if possible. Otherwise, returns '1'.
+        /// </summary>
         public static int Square(int length)
         {
-            if (IsSquarable(length)) return (int)Math.Sqrt(length);
+            if (IsRootable(length)) return (int)Math.Sqrt(length);
             Console.WriteLine($"Length {length} not divisible by two (2)");
             return 1;
         }
