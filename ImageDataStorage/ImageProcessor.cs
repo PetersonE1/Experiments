7f0ffb57364314ef6009ccd98e756226be2a46ca
PetersonE1 <PetersonE_1@live.com>
2022-11-03 18:05:19 -0400

    image processor major upgrades

---
+++ ImageDataStorage/ImageProcessor.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ImageDataStorage
+{
+    public static class ImageProcessor
+    {
+        // Constants
+        public const uint X1 = 256u;
+        public const uint X2 = 256u * 256u;
+        public const uint X3 = 256u * 256u * 256u;
+
+        // Item Conversions
+        static uint ConvertToInt(this Color c)
+        {
+            uint result = 0;
+            result += c.R;
+            result += c.G * X1;
+            result += c.B * X2;
+            result += c.A * X3;
+            return result;
+        }
+
+        static Color ConvertToColor(this uint i)
+        {
+            int R, G, B, A = 0;
+
+            A = (int)(i / X3);
+            i -= X3 * (uint)A;
+            B = (int)(i / X2);
+            i -= X2 * (uint)B;
+            G = (int)(i / X1);
+            i -= X1 * (uint)G;
+            R = (int)(i);
+
+            Color c = Color.FromArgb(A, R, G, B);
+            return c;
+        }
+
+        // Data Conversions
+        public static uint[] ConvertImageToArray(string filePath)
+        {
+            if (!File.Exists(filePath)) return null;
+
+            Bitmap image = new Bitmap(filePath);
+            Console.WriteLine(image.Height);
+            int imageSize = image.Height * image.Width;
+            uint[] imageData = new uint[imageSize];
+
+            int i = 0;
+            for (int h = 0; h < image.Height; h++)
+            {
+                for (int w = 0; w < image.Width; w++)
+                {
+                    Color c = image.GetPixel(w, h);
+                    imageData[i] = image.GetPixel(w, h).ConvertToInt();
+                    i++;
+                }
+            }
+            image.Dispose();
+
+            return imageData;
+        }
+
+        public static Bitmap ConvertToImage(uint[] data) {
+            List<int> heights = new List<int>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (IsValid(i, data.Length)) heights.Add(i);
+            }
+
+            Console.WriteLine($"The available heights are: {string.Join(", ", heights)}");
+            Console.Write("Choose a height: ");
+            string result;
+            while (!ReadIfContains(heights.Select(s => s.ToString()).ToArray(), out result)) {
+                Console.Write("\n Please enter a valid height: ");
+            }
+            int height = Convert.ToInt32(result);
+
+            int width = data.Length / height;
+            Bitmap map = new Bitmap(width, height);
+            
+            int index = 0;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    map.SetPixel(w, h, data[index].ConvertToColor());
+                    index++;
+                }
+            }
+
+            return map;
+        }
+
+        public static Bitmap ConvertToImage(uint[] data, int attemptHeight)
+        {
+            List<int> heights = new List<int>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (IsValid(i, data.Length)) heights.Add(i);
+            }
+
+            if (!heights.Contains(attemptHeight)) attemptHeight = heights[0];
+            int height = attemptHeight;
+
+            int width = data.Length / height;
+            Bitmap map = new Bitmap(width, height);
+
+            int index = 0;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    map.SetPixel(w, h, data[index].ConvertToColor());
+                    index++;
+                }
+            }
+
+            return map;
+        }
+
+        // Wrapped Methods
+        public static void GenerateImageFromFile(string inputPath, string outputPath)
+        {
+            if (!File.Exists(inputPath)) return;
+            string raw_data = File.ReadAllText(inputPath);
+            uint[] array = raw_data.Split('\n').Select(n => Convert.ToUInt32(n)).ToArray();
+            Bitmap map = ConvertToImage(array);
+            map.Save(outputPath + $"-H{map.Height}.png");
+        }
+
+        public static void GenerateImageFromFile(string inputPath, string outputPath, int targetHeight)
+        {
+            if (!File.Exists(inputPath)) return;
+            string raw_data = File.ReadAllText(inputPath);
+            uint[] array = raw_data.Split('\n').Select(n => Convert.ToUInt32(n)).ToArray();
+            Bitmap map = ConvertToImage(array, targetHeight);
+            map.Save(outputPath + $"-H{map.Height}.png");
+        }
+
+        public static void GenerateImageFromArray(uint[] array, string outputPath)
+        {
+            Bitmap map = ConvertToImage(array);
+            map.Save(outputPath + $"-H{map.Height}.png");
+        }
+
+        public static void GenerateImageFromArray(uint[] array, string outputPath, int targetHeight)
+        {
+            Bitmap map = ConvertToImage(array, targetHeight);
+            map.Save(outputPath + $"-H{map.Height}.png");
+        }
+
+        public static void SaveArrayToFile(uint[] array, string outputPath)
+        {
+            string data = string.Join('\n', array);
+            File.WriteAllText(outputPath, data);
+        }
+
+        // Data Processing
+        private static bool ReadIfContains(string[] values, out string n)
+        {
+            n = Console.ReadLine();
+            return values.Contains(n);
+        }
+
+        public static int AskHeight(int length)
+        {
+            List<int> heights = new List<int>();
+            for (int i = 0; i < length; i++)
+            {
+                if (IsValid(i, length)) heights.Add(i);
+            }
+
+            Console.WriteLine($"The available heights are: {string.Join(", ", heights)}");
+            Console.Write("Choose a height: ");
+            string result;
+            while (!ReadIfContains(heights.Select(s => s.ToString()).ToArray(), out result))
+            {
+                Console.Write("\n Please enter a valid height: ");
+            }
+            int height = Convert.ToInt32(result);
+            return height;
+        }
+
+        // Bonus Methods
+        public static bool IsFactor(int a, int b)
+        {
+            if (a != 0 && b % a == 0) return true;
+            return false;
+        }
+
+        public static bool IsValid(int a, int b)
+        {
+            if (IsFactor(a, b) && a < 65536 && b / a < 65536) return true;
+            return false;
+        }
+
+        public static bool IsSquarable(int a)
+        {
+            int b = (int)Math.Sqrt(a);
+            if (a == b * b) return true;
+            return false;
+        }
+
+        public static Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(sourceBMP, 0, 0, width, height);
+            }
+            return result;
+        }
+
+        public static int Square(int length)
+        {
+            if (IsSquarable(length)) return (int)Math.Sqrt(length);
+            Console.WriteLine($"Length {length} not divisible by two (2)");
+            return 1;
+        }
+    }
+}
---
+++ ImageDataStorage/Runner.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ImageDataStorage
+{
+    public static class Runner
+    {
+        const int length = 10000;
+
+        static void Main(string[] args)
+        {
+            Fibonacci();
+        }
+
+        static private void Quadratic(bool askForHeight = false)
+        {
+            int height = ImageProcessor.Square(length);
+            uint a = 123;
+            uint b = 456;
+            uint c = 789;
+
+
+            if (!askForHeight && File.Exists($"outputs/quadratic-{length}-{a}-{b}-{c}-H{height}.png")) {Console.WriteLine("File already exists"); return; }
+
+            uint[] array = new uint[length];
+            for (uint x = 0; x < length; x++)
+            {
+                array[x] = a * (x * x) + b * x + c;
+            }
+
+            if (askForHeight) { height = ImageProcessor.AskHeight(length); if (File.Exists($"outputs/quadratic-{length}-{a}-{b}-{c}-H{height}.png")) { Console.WriteLine("File already exists"); return; } }
+
+            if (!File.Exists($"inputs/arrays/quadratic-{length}-{a}-{b}-{c}")) ImageProcessor.SaveArrayToFile(array, $"inputs/arrays/quadratic-{length}-{a}-{b}-{c}");
+            ImageProcessor.GenerateImageFromArray(array, $"outputs/quadratic-{length}-{a}-{b}-{c}", height);
+        }
+
+        static private void Cubic(bool askForHeight = false)
+        {
+            int height = ImageProcessor.Square(length);
+            uint a = 12;
+            uint b = 34;
+            uint c = 56;
+            uint d = 78;
+
+            if (askForHeight)
+            if (!askForHeight && File.Exists($"outputs/cubic-{length}-{a}-{b}-{c}-{d}-H{height}.png")) { Console.WriteLine("File already exists"); return; }
+
+            uint[] array = new uint[length];
+            for (uint x = 0; x < length; x++)
+            {
+                array[x] = a * (x * x * x) + b * (x * x) + c * x + d;
+            }
+
+            if (askForHeight) { height = ImageProcessor.AskHeight(length); if (File.Exists($"outputs/cubic-{length}-{a}-{b}-{c}-{d}-H{height}.png")) { Console.WriteLine("File already exists"); return; } }
+
+            if (!File.Exists($"inputs/arrays/cubic-{length}-{a}-{b}-{c}-{d}")) ImageProcessor.SaveArrayToFile(array, $"inputs/arrays/cubic-{length}-{a}-{b}-{c}-{d}");
+            ImageProcessor.GenerateImageFromArray(array, $"outputs/cubic-{length}-{a}-{b}-{c}-{d}", height);
+        }
+
+        static private void Fibonacci(bool askForHeight = false)
+        {
+            int height = ImageProcessor.Square(length);
+
+
+            if (!askForHeight && File.Exists($"outputs/fibonacci-{length}-H{height}.png")) { Console.WriteLine("File already exists"); return; }
+
+            uint num1 = 0;
+            uint num2 = 1;
+            uint[] array = new uint[length];
+            for (uint i = 0; i < length; i++)
+            {
+                array[i] = num2;
+                uint temp = num2;
+                num2 += num1;
+                num1 = temp;
+            }
+
+            if (askForHeight) { height = ImageProcessor.AskHeight(length); if (File.Exists($"outputs/fibonacci-{length}-H{height}.png")) { Console.WriteLine("File already exists"); return; } }
+
+            if (!File.Exists($"inputs/arrays/fibonacci-{length}")) ImageProcessor.SaveArrayToFile(array, $"inputs/arrays/fibonacci-{length}");
+            ImageProcessor.GenerateImageFromArray(array, $"outputs/fibonnaci-{length}", height);
+        }
+    }
+}
