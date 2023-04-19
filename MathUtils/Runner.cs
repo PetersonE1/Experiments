05c180cf25acf37c21d85a1a42a78b20faf64703
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-04-19 16:39:36 -0400

    midi tools - string to morse code

--- MathUtils/Runner.cs
+++ MathUtils/Runner.cs
@@ -1,10 +1,21 @@
-﻿namespace MathUtils
+﻿using Microsoft.Win32;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace MathUtils
 {
     public static class Runner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine(RuntimeInformation.FrameworkDescription);
+            Console.WriteLine(RuntimeEnvironment.GetSystemVersion());
+            bool[] bools = new bool[] { false, true, false };
+            BitArray arr = new BitArray(bools);
+            foreach (bool i in arr)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
---
+++ MidiUtils/Runner.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace MidiUtils
+{
+    public static class Runner
+    {
+        static void Main(string[] args)
+        {
+            MidiTools.StringToMorseMidi("Test message, please ignore", 75);
+        }
+    }
+}
