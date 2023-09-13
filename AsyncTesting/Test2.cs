b936611afa3526f4124d25068fa5baa73af1b04d
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-13 13:23:02 -0400

    added async testing project

---
+++ AsyncTesting/Test2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTesting
+{
+    internal static class Test2
+    {
+        public static async Task<double> Run(int iterations)
+        {
+            DateTime startTime = DateTime.Now;
+
+            int t = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                await Task.Run(() => t += i);
+            }
+
+            Console.WriteLine(t);
+            return (DateTime.Now - startTime).TotalMilliseconds;
+        }
+    }
+}
