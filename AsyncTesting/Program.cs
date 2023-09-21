311e7045542001a0ef712c974be1a9fc2c0a408e
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-21 13:16:40 -0400

    installed Benchmark.NET to test sequence testing class, shows overhead of threads is detrimental for this type of function (by a lot)

--- AsyncTesting/Program.cs
+++ AsyncTesting/Program.cs
@@ -1,39 +1,4 @@
 ﻿using AsyncTesting;
-using (FileStream fs = new FileStream("outputLog.txt", FileMode.Create, FileAccess.Write))
-{
-    using StreamWriter writer = new StreamWriter(fs);
-    Console.SetOut(writer);
-    Console.WriteLine("Test: i = 0; i < iteration; i++; t += i; Console.WriteLine(t)");
-    
-    Console.WriteLine("\nIterations: 1000");
-    Console.WriteLine("Running synchronous test");
-    Console.WriteLine(Test1.Run(1000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running awaited asynchronous test");
-    Console.WriteLine(await Test2.Run(1000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running true asynchronous test");
-    Console.WriteLine(Test3.Run(1000) + " ms");
-    
-    Console.WriteLine("\nIterations: 10000");
-    Console.WriteLine("Running synchronous test");
-    Console.WriteLine(Test1.Run(10000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running awaited asynchronous test");
-    Console.WriteLine(await Test2.Run(10000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running true asynchronous test");
-    Console.WriteLine(Test3.Run(10000) + " ms");
-    
-    Console.WriteLine("\nIterations: 100000");
-    Console.WriteLine("Running synchronous test");
-    Console.WriteLine(Test1.Run(100000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running awaited asynchronous test");
-    Console.WriteLine(await Test2.Run(100000) + " ms");
-    Console.WriteLine("-----------------------------------");
-    Console.WriteLine("Running true asynchronous test");
-    Console.WriteLine(Test3.Run(100000) + " ms");
-}
+using BenchmarkDotNet.Running;
 
-
+var summary = BenchmarkRunner.Run<SequenceTest>();
---
+++ AsyncTesting/SequenceTest.cs
@@ -0,0 +1,65 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTesting
+{
+    [MemoryDiagnoser]
+    public class SequenceTest
+    {
+        [Params(100, 1000, 10000)]
+        public int iterations;
+
+        [Benchmark]
+        public double Run1()
+        {
+            DateTime startTime = DateTime.Now;
+
+            int t = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                t += i;
+            }
+
+            return (DateTime.Now - startTime).TotalMilliseconds;
+        }
+
+        [Benchmark]
+        public async Task<double> Run2()
+        {
+            DateTime startTime = DateTime.Now;
+
+            int t = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                await Task.Run(() => t += i);
+            }
+
+            return (DateTime.Now - startTime).TotalMilliseconds;
+        }
+
+        [Benchmark]
+        public double Run3()
+        {
+            DateTime startTime = DateTime.Now;
+
+            List<Task> tasks = new List<Task>();
+
+            int t = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                tasks.Add(Task.Run(() => t += i));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            return (DateTime.Now - startTime).TotalMilliseconds;
+        }
+    }
+}
